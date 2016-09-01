3c6b23018e5a9d8df880796205baa1370885fd7e
Florent Vilmart <florentvilmart@me.com>
2016-09-01 00:08:06 -0400

    Reverts calling next() after response

--- src/PromiseRouter.js
+++ src/PromiseRouter.js
@@ -23,7 +23,6 @@ export default class PromiseRouter {
   //     location: optional. a location header
   constructor(routes = [], appId) {
     this.routes = routes;
-    this.middlewares = [];
     this.appId = appId;
     this.mountRoutes();
   }
@@ -38,10 +37,6 @@ export default class PromiseRouter {
       this.routes.push(route);
     }
   };
-
-  use(middleware) {
-    this.middlewares.push(middleware);
-  }
 
   route(method, path, ...handlers) {
     switch(method) {
@@ -115,8 +110,7 @@ export default class PromiseRouter {
     this.routes.forEach((route) => {
       let method = route.method.toLowerCase();
       let handler = makeExpressHandler(this.appId, route.handler);
-      let args = [].concat(route.path, this.middlewares, handler);
-      expressApp[method].apply(expressApp, args);
+      expressApp[method].call(expressApp, route.path, handler);
     });
     return expressApp;
   };
